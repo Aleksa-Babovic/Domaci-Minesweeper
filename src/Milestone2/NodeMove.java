8a1697318ba392fb71794bfc4ce959f055c8145f
Aleksa-Babovic <78506502+Aleksa-Babovic@users.noreply.github.com>
2026-05-19 01:45:48 +0200

    Add files via upload

---
+++ src/Milestone2/NodeMove.java
@@ -0,0 +1,11 @@
+package Milestone2;
+
+public class NodeMove {
+    Move data;
+    NodeMove next;
+
+    public NodeMove(Move data) {
+        this.data = data;
+        this.next = null;
+    }
+}
